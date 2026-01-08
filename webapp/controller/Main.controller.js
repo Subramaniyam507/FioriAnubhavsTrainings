4c3b6f777a787c0e1b95800be4603531fd1e5b5d
Subramaniyam N <subramaniyam96@gmail.com>
2026-01-08 03:33:26 +0000

    14 Basics of Model

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,17 +1,58 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(
-	Controller
+    "sap/ui/core/mvc/Controller"
+], function (
+    Controller
 ) {
-	"use strict";
+    "use strict";
 
-	return Controller.extend("mickey.controller.Main", {
+    return Controller.extend("mickey.controller.Main", {
+        oView: null,
+        onInit() {
+            this.oView = this.getView()
+            var oModel = new sap.ui.model.json.JSONModel()
+            var data = {
+                "empStr":{
+                    "empId": 5000,
+                    "empName": "Anubhav",
+                    "salary": 9000,
+                    "currency": "USD",
+                    "smoker":false,
 
-
-        myCode:function(){
+                },
+                "empTable":[
+                    {
+                    "empId": 1000,
+                    "empName": "Amrut",
+                    "salary": 4000,
+                    "currency": "USD",
+                    "smoker":true,
+                    
+                },
+                {
+                    "empId": 5001,
+                    "empName": "Bilal",
+                    "salary": 8023,
+                    "currency": "EURO",
+                    "smoker":false,
+                    
+                },
+                {
+                    "empId": 5330,
+                    "empName": "Shahid",
+                    "salary": 11000,
+                    "currency": "INR",
+                    "smoker":true,
+                    
+                }
+                ]
+            }
+            oModel.setData(data)
+            sap.ui.getCore().setModel(oModel)
+        },
+        myCode: function () {
 
             var oInput = sap.ui.getCore().byId("idInp");
-            if(!oInp.getValue()){
+            if (!oInp.getValue()) {
                 alert("please enter a value")
                 return;
             }
@@ -22,14 +63,30 @@ sap.ui.define([
             }
         },
 
-		onClickButton: function(oEvent) {
-			var oView = this.getView();
-            var onInp = oView.byId("idInputOne");
+        onClickButton: function (oEvent) {
+
+            var onInp = this.oView.byId("idInputOne");
             var sValue = onInp.getValue();
             alert(sValue)
-		},
+        },
 
-	});
+        onPrint: function (oEvent) {
+            var idEmpid = this.oView.byId("idEmpid");
+            var idEmpName = this.oView.byId("idEmpName");
+            var idSal = this.oView.byId("idSal");
+            var idCurr = this.oView.byId("idCurr");
+            var idSmoker = this.oView.byId("idSmoker")
+
+            alert(
+                idEmpid.getValue(),
+                idEmpName.getValue(),
+                idSal.getValue(),
+                idCurr.getValue(),
+                idSmoker.getSelected()
+            );
+        }
+
+    });
 
 
 });
